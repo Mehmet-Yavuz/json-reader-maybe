2de0cb2cf8d3969cedad14677b97febd7f82decf
Mehmet Yavuz <mehmet.yavuz@zappware.com>
2022-05-10 14:25:10 +0200

    Add reader class

---
+++ reader.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const appConfig = require('./appcfg_stb_features.json');
+let tab = 0;
+
+console.log(JSON.stringify(appConfig, null, 5));
+
+// featureFileReader(appConfig);
+
+function exitHandler(next) {
+    fs.writeFile("config.json", JSON.stringify(appConfig), 'utf8', function (err) {
+        if (err) {
+            next(err);
+        }
+     
+        console.log("JSON file has been saved.");
+        process.exit(0);
+    });
+}
+
+function featureFileReader(file) {
+    for (const feature in file){
+        console.log(feature);
+        if(typeof(file[feature]) === "object"){
+            tab++;
+            featureFileReader(file[feature])
+        }else{
+            console.log(file[feature] + " tab " + tab)
+        }
+    }
+}
